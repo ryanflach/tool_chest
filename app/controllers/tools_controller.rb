c20a630a6525909274c0fe954206b92452237feb
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-13 12:38:47 -0600

    Complete functionality

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,51 @@
+class ToolsController < ApplicationController
+  def index
+    @tools = Tool.all
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tools_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def edit
+    @tool = tool_for_page
+  end
+
+  def show
+    @tool = tool_for_page
+  end
+
+  def update
+    @tool = tool_for_page
+    if @tool.update_attributes(tool_params)
+      redirect_to tools_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tool = tool_for_page
+    @tool.destroy 
+    redirect_to tools_path
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :quantity, :price)
+  end
+
+  def tool_for_page
+    Tool.find(params[:id])
+  end
+end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,2 +1,5 @@
 class Tool < ActiveRecord::Base
+  validates :name, presence: true
+  validates :price, presence: true, uniqueness: { scope: :name }
+  validates :quantity, presence: true
 end
