a12b2f2a1608c9bc4081a71028454cc1e86c2efb
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-20 13:58:13 -0600

    Add index to username field of User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719205202) do
+ActiveRecord::Schema.define(version: 20160720195654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20160719205202) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "tools", "categories"
   add_foreign_key "tools", "users"
